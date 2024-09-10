04f091ab5444696686385ea315a4a969249d490d
adrianvilla1504 <105081872+AdrianVilla1504@users.noreply.github.com>
2024-09-10 13:45:04 -0500

    changes: delete opertation and clean operation

--- src/hooks/useCalculator.tsx
+++ src/hooks/useCalculator.tsx
@@ -4,10 +4,54 @@ export const useCalculator = () => {
   const [number, setNumber] = useState('0');
   const buildNumber = (numberString: string) => {
     setNumber(number + numberString);
+    if (number.includes('.') && numberString === '.') return;
+    if (number.startsWith('0') || number.startsWith('-0')) {
+      if (numberString === '.') {
+        return setNumber(number + numberString);
+      }
+      if (numberString === '0' && number.includes('.')) {
+        return setNumber(number + numberString);
+      }
+      if (numberString !== '0' && !number.includes('.')) {
+        return setNumber(numberString);
+      }
+      if (numberString === '0' && !number.includes('.')) {
+        return;
+      }
+      return setNumber(number + numberString);
+    }
+    setNumber(number + numberString);
   };
+
+  const clean = () => {
+    setNumber('0');
+  };
+  const deleteOperation = () => {
+    let currentSign = '';
+    let temporalNumber = number;
+    if (number.includes('-')) {
+      currentSign = '-';
+      temporalNumber = number.substring(1);
+    }
+    if (temporalNumber.length > 1) {
+      return setNumber(currentSign + temporalNumber.slice(0, -1));
+    }
+    setNumber('0');
+  };
+  const toggleSign = () => {
+    if (number.includes('-')) {
+      return setNumber(number.replace('-', ''));
+    }
+    setNumber('-' + number);
+  };
+
   return {
     number,
     buildNumber,
+    setNumber,
+    clean,
+    deleteOperation,
+    toggleSign,
   };
 };
 
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -6,16 +6,23 @@ import {styles} from '../../config/theme/app-theme';
 import useCalculator from '../../hooks/useCalculator';
 import {CalculatorButton} from '../../components/CalculatorButton';
 export const CalculatorScreen = () => {
-  const {number, buildNumber} = useCalculator();
+  const {number, buildNumber, clean, deleteOperation, toggleSign} =
+    useCalculator();
+
   return (
     <View style={styles.calculatorContainer}>
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
-        <Text style={styles.mainResult}> {number} </Text>
+        <Text
+          adjustsFontSizeToFit={true}
+          numberOfLines={1}
+          style={styles.mainResult}>
+          {number}
+        </Text>
         <Text style={styles.subResult}> {number} </Text>
       </View>
       <View style={styles.row}>
         <CalculatorButton
-          onPress={() => console.log('C')}
+          onPress={() => clean()}
           blackText
           label="C"
           color="LightGray"
@@ -27,7 +34,7 @@ export const CalculatorScreen = () => {
           color="LightGray"
         />
         <CalculatorButton
-          onPress={() => console.log('del')}
+          onPress={() => deleteOperation()}
           blackText
           label="del"
           color="LightGray"
@@ -39,9 +46,9 @@ export const CalculatorScreen = () => {
         />
       </View>
       <View style={styles.row}>
-        <CalculatorButton onPress={() => console.log('7')} label="7" />
-        <CalculatorButton onPress={() => console.log('8')} label="8" />
-        <CalculatorButton onPress={() => console.log('9')} label="9" />
+        <CalculatorButton onPress={() => buildNumber('7')} label="7" />
+        <CalculatorButton onPress={() => buildNumber('8')} label="8" />
+        <CalculatorButton onPress={() => buildNumber('9')} label="9" />
         <CalculatorButton
           onPress={() => console.log('÷')}
           label="÷"
@@ -49,19 +56,19 @@ export const CalculatorScreen = () => {
         />
       </View>
       <View style={styles.row}>
-        <CalculatorButton onPress={() => console.log('4')} label="4" />
-        <CalculatorButton onPress={() => console.log('5')} label="5" />
-        <CalculatorButton onPress={() => console.log('6')} label="6" />
+        <CalculatorButton onPress={() => buildNumber('4')} label="4" />
+        <CalculatorButton onPress={() => buildNumber('5')} label="5" />
+        <CalculatorButton onPress={() => buildNumber('6')} label="6" />
         <CalculatorButton
-          onPress={() => console.log('-')}
+          onPress={() => buildNumber('-')}
           label="-"
           color="orange"
         />
       </View>
       <View style={styles.row}>
-        <CalculatorButton onPress={() => console.log('1')} label="1" />
-        <CalculatorButton onPress={() => console.log('2')} label="2" />
-        <CalculatorButton onPress={() => console.log('3')} label="3" />
+        <CalculatorButton onPress={() => buildNumber('1')} label="1" />
+        <CalculatorButton onPress={() => buildNumber('2')} label="2" />
+        <CalculatorButton onPress={() => buildNumber('3')} label="3" />
         <CalculatorButton
           onPress={() => console.log('+')}
           label="+"
@@ -70,11 +77,11 @@ export const CalculatorScreen = () => {
       </View>
       <View style={styles.row}>
         <CalculatorButton
-          onPress={() => console.log('0')}
+          onPress={() => buildNumber('0')}
           label="0"
           doubleSize={true}
         />
-        <CalculatorButton onPress={() => console.log('.')} label="." />
+        <CalculatorButton onPress={() => buildNumber('.')} label="." />
         <CalculatorButton
           onPress={() => console.log('=')}
           label="="
